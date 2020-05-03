d18f9d19938f274ccf149ebe3daef953461bb5b5
sage <laymonage@gmail.com>
2020-05-03 15:21:36 +0700

    Make Contact component bigger
    Bigger is better ;)

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="rounded bg-white p-8
-  flex-col xl:w-3/4 w-full mx-auto shadow-md">
-    <div class="mb-8">
-      <div class="flex items-end text-left text-blue-700 font-bold text-xl mb-2">
-        Sage M. Abdullah
-        <div class="font-normal text-lg xs:text-right">
-          <span class="text-gray-500 ml-2">a.k.a. </span>
-          <span>laymonage</span>
+  flex-col xl:w-5/6 w-full mx-auto shadow-md">
+    <div>
+      <div class="flex items-baseline xs:items-center
+      text-left text-blue-700 font-bold mb-2
+      md:text-5xl text-4xl">
+        laymonage
+        <div class="font-normal md:text-3xl text-2xl xs:text-right">
+          <span class="text-gray-500 ml-4 mr-2">is</span>
+          <span>Sage M. Abdullah</span>
         </div>
       </div>
-      <p class="text-gray-700 text-left">
-        <span class="font-semibold">Computer Science</span>
-        student at Universitas Indonesia
-        and an <span class="font-semibold">open source</span> enthusiast.
-      </p>
+    </div>
+    <div class="text-gray-700 text-left text-2xl mt-6 mb-8">
+      <span class="font-semibold">Computer Science</span>
+      student at Universitas Indonesia
+      and an <span class="font-semibold">open source</span> enthusiast.
     </div>
     <div class="flex items-center">
       <a href="https://github.com/laymonage">
