64a1f9774f956de13c7fdb496eca938d9460b134
sage <laymonage@gmail.com>
2020-05-03 21:41:32 +0700

    Use Education component in Profile view

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="mx-auto">
-    <h1 class="text-6xl">Coming soon!</h1>
+  <div class="container mx-auto px-4 items-center">
+    <Education />
   </div>
 </template>
+
+<script>
+import Education from '@/components/Education.vue';
+
+export default {
+  name: 'Profile',
+  components: {
+    Education,
+  },
+};
+</script>
