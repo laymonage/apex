b84dfb2964ffd5705a7e3db819edc7fbb34d5f9b
sage <laymonage@gmail.com>
2020-05-20 20:12:03 +0700

    Hide Navbar on scroll down

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,64 @@
 <template>
-  <div id="nav" class="z-10 md:fixed p-8 mb-4">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/profile">Profile</router-link>
+  <div
+    class="flex navbar z-10 fixed p-4 bg-white shadow-md
+    transition-all duration-300 ease-out"
+    :class="{ 'navbar-hidden': !showNavbar }">
+    <router-link to="/">Home</router-link>
+    <router-link class="ml-4" to="/profile">Profile</router-link>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+function onScroll() {
+  const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScrollPosition < 0) {
+    return;
+  }
+  if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60) {
+    return;
+  }
+  this.showNavbar = currentScrollPosition < this.lastScrollPosition;
+  this.lastScrollPosition = currentScrollPosition;
+}
+
 export default Vue.extend({
   name: 'Navbar',
   components: {},
+  data() {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener('scroll', this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll);
+  },
+  methods: {
+    onScroll,
+  },
 });
 </script>
 
 <style scoped>
-@media (max-height: 540px) {
-  #nav {
-    position: initial;
-  }
+.navbar {
+  transform: translate3d(0, 0, 0);
 }
 
-#nav a {
+.navbar.navbar-hidden {
+  @apply shadow-none;
+  transform: translate3d(0, -100%, 0);
+}
+
+.navbar a {
   @apply font-bold text-blue-900;
 }
 
-#nav a.router-link-exact-active {
-  @apply text-blue-700;
+.navbar a.router-link-exact-active {
+  @apply text-blue-600;
 }
 </style>
