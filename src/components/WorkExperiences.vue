f403bf96e88aae6725c2f1cd30dbe66265d43d29
sage <laymonage@gmail.com>
2020-05-21 21:40:25 +0700

    Extract Timeline component

--- src/components/WorkExperiences.vue
+++ src/components/WorkExperiences.vue
@@ -5,106 +5,28 @@
       Work Experiences
     </template>
 
-    <div
-      v-for="(exp, expIndex) in workExperiences.data"
-      v-bind:key="`exp-${expIndex}`"
-      class="flex flex-col md:flex-row-reverse justify-between text-gray-700">
-      <div class="flex flex-col items-center mb-8 md:mb-0 md:w-3/12 lg:w-2/12">
-        <div class="mb-4 md:mb-2">
-          <img
-            :alt="exp.employer"
-            :src="exp.employerLogo.image"
-            :class="exp.employerLogo.class"
-            class="w-1/2 ph:w-3/4 md:w-auto mx-auto">
-        </div>
-        <div class="flex items-center">
-          <span>{{ exp.dates.start }}</span>
-          <span v-if="exp.dates.end !== exp.dates.start">
-            <span class="mx-1">-</span>
-            <span>{{ exp.dates.end }}</span>
-          </span>
-        </div>
-        <div
-          v-if="expIndex !== workExperiences.data.length - 1"
-          class="hidden md:flex w-1/4 h-full">
-          <div class="border-l border-solid border-4 mx-auto my-2"></div>
-        </div>
-      </div>
-      <div class="flex flex-col items-start md:w-3/4 md:mr-8 md:mb-8">
-        <div class="flex flex-col">
-          <div class="flex flex-col text-xl ph:text-lg">
-            <span>
-              <span v-html="sanitize(marked(exp.title))"></span>
-            </span>
-            <span>{{ exp.employer }}</span>
-          </div>
-        </div>
-        <div class="flex flex-col mt-4">
-          <span>
-            {{ exp.description }}
-          </span>
-          <component
-            v-if="exp.details"
-            :is="exp.details.type === 'decimal' ? 'ol' : 'ul'"
-            :class="
-              exp.details.type === 'decimal' ? 'list-decimal' :
-              exp.details.type === 'disc' ? 'list-disc' :
-              'list-none'
-            "
-            class="ml-6 p-2">
-            <li
-              v-for="(child, childIndex) in exp.details.children"
-              v-bind:key="`exp-${expIndex}-${childIndex}`"
-              v-html="sanitize(marked(child))">
-            </li>
-          </component>
-        </div>
-      </div>
-      <div
-        v-if="expIndex !== workExperiences.data.length - 1"
-        class="block md:hidden w-full">
-        <div class="border-b border-solid border mx-auto my-16"></div>
-      </div>
-    </div>
+    <Timeline :items="workExperiences" />
 
   </BaseCard>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import marked from 'marked';
-import { sanitize } from 'dompurify';
-import workExperiences from '@/data/work-experiences';
+import Timeline from '@/components/profile/Timeline.vue';
+import WorkExperiences from '@/data/work-experiences';
 
 export default Vue.extend({
   name: 'WorkExperiences',
-  components: {},
+  components: {
+    Timeline,
+  },
   data() {
     return {
-      workExperiences,
+      workExperiences: WorkExperiences.data,
     };
-  },
-  methods: {
-    marked,
-    sanitize,
   },
 });
 </script>
 
 <style scoped>
-ul >>> a {
-  @apply font-semibold text-blue-700;
-}
-
-ol.list-decimal >>> ul {
-  @apply list-disc ml-8;
-}
-
-ol.list-decimal {
-  @apply font-semibold;
-}
-
-ol.list-decimal >>> p, ol.list-decimal >>> ul {
-  @apply font-normal;
-}
 </style>
---
+++ src/components/profile/Timeline.vue
@@ -0,0 +1,99 @@
+<template>
+  <ol>
+    <li
+      v-for="(item, itemIndex) in items"
+      v-bind:key="`item-${itemIndex}`"
+      class="flex flex-col md:flex-row-reverse justify-between text-gray-700">
+      <div class="flex flex-col items-center mb-8 md:mb-0 md:w-3/12 lg:w-2/12">
+        <div class="mb-4 md:mb-2">
+          <img
+            :alt="item.employer"
+            :src="item.employerLogo.image"
+            :class="item.employerLogo.class"
+            class="w-1/2 ph:w-3/4 md:w-auto mx-auto">
+        </div>
+        <div class="flex items-center">
+          <span>{{ item.dates.start }}</span>
+          <span v-if="item.dates.end !== item.dates.start">
+            <span class="mx-1">-</span>
+            <span>{{ item.dates.end }}</span>
+          </span>
+        </div>
+        <div
+          v-if="itemIndex !== items.length - 1"
+          class="hidden md:flex w-1/4 h-full">
+          <div class="border-l border-solid border-4 mx-auto my-2"></div>
+        </div>
+      </div>
+      <div class="flex flex-col items-start md:w-3/4 md:mr-8 md:mb-8">
+        <div class="flex flex-col">
+          <div class="flex flex-col text-xl ph:text-lg">
+            <span>
+              <span v-html="sanitize(marked(item.title))"></span>
+            </span>
+            <span>{{ item.employer }}</span>
+          </div>
+        </div>
+        <div class="flex flex-col mt-4">
+          <span>
+            {{ item.description }}
+          </span>
+          <component
+            v-if="item.details"
+            :is="item.details.type === 'decimal' ? 'ol' : 'ul'"
+            :class="
+              item.details.type === 'decimal' ? 'list-decimal' :
+              item.details.type === 'disc' ? 'list-disc' :
+              'list-none'
+            "
+            class="ml-6 p-2">
+            <li
+              v-for="(child, childIndex) in item.details.children"
+              v-bind:key="`item-${itemIndex}-${childIndex}`"
+              v-html="sanitize(marked(child))">
+            </li>
+          </component>
+        </div>
+      </div>
+      <div
+        v-if="itemIndex !== items.length - 1"
+        class="block md:hidden w-full">
+        <div class="border-b border-solid border mx-auto my-16"></div>
+      </div>
+    </li>
+  </ol>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import marked from 'marked';
+import { sanitize } from 'dompurify';
+
+export default Vue.extend({
+  name: 'Timeline',
+  components: {},
+  methods: {
+    marked,
+    sanitize,
+  },
+  props: ['items'],
+});
+</script>
+
+<style scoped>
+ul >>> a {
+  @apply font-semibold text-blue-700;
+}
+
+ol.list-decimal >>> ul {
+  @apply list-disc ml-8;
+}
+
+ol.list-decimal {
+  @apply font-semibold;
+}
+
+ol.list-decimal >>> p, ol.list-decimal >>> ul {
+  @apply font-normal;
+}
+</style>
