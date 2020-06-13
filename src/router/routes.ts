85d8732753eaa5f86f7f64bb5854f00ce42cd247
sage <laymonage@gmail.com>
2020-06-13 20:51:55 +0700

    Assign routes' meta title property programmatically

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,44 +1,41 @@
+import { Route } from 'vue-router';
 import Home from '@/views/Home.vue';
 import NotFound from '@/views/NotFound.vue';
 
 const DEFAULT_TITLE = 'laymonage';
 
-const makeTitle = (title: string) => `${title} | ${DEFAULT_TITLE}`;
+const title = (to: Route) => (
+  (to.name === 'Home' && DEFAULT_TITLE)
+  || `${to.name} | ${DEFAULT_TITLE}`
+);
 
 export const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
-    meta: {
-      title: () => DEFAULT_TITLE,
-    },
   },
   {
     path: '/profile',
     name: 'Profile',
     component: () => import('@/views/Profile.vue' /* webpackChunkName: "profile" */),
-    meta: {
-      title: () => makeTitle('Profile'),
-    },
   },
   {
     path: '/projects',
     name: 'Projects',
     component: () => import('@/views/Projects.vue' /* webpackChunkName: "projects" */),
-    meta: {
-      title: () => makeTitle('Projects'),
-    },
   },
   {
     path: '/*',
     name: 'Not Found',
     component: NotFound,
-    meta: {
-      title: () => makeTitle('Not Found'),
-    },
   },
-];
+].map((route) => ({
+  ...route,
+  meta: {
+    title,
+  },
+}));
 
 export default {
   routes,
