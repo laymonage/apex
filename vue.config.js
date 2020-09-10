8dbe048ba6f72cea5eed0dd11f0400824500be67
sage <laymonage@gmail.com>
2020-09-10 18:58:59 +0700

    Disable prerendering
    We'll try doing it from Netlify instead, maybe the light flash won't be so bad.

--- vue.config.js
+++ vue.config.js
@@ -18,26 +18,4 @@ module.exports = {
       .use('file-loader')
       .loader('file-loader');
   },
-
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/',
-        '/profile',
-        '/projects',
-        '/blog',
-        '/404',
-      ],
-      postProcess: (route) => {
-        route.html = route.html // eslint-disable-line no-param-reassign
-          .replace(/<script (.*?)>/g, '<script $1 defer>')
-          .replace('id="app"', 'id="app" data-server-rendered="true"');
-        return route;
-      },
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true,
-    },
-  },
 };
